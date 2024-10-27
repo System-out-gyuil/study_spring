4ab1498769acf547047471b1769771502a67aa1b
dev_gyuil <a46884334@gmail.com>
2024-10-27 17:30:50 +0900

    board done

---
+++ src/main/java/com/example/projectboard/repository/UserAccountRepository.java
@@ -0,0 +1,5 @@
+package com.example.projectboard.repository;
+import com.example.projectboard.domain.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
+}
---
+++ src/main/java/com/example/projectboard/repository/querydsl/ArticleRepositoryCustom.java
@@ -0,0 +1,5 @@
+package com.example.projectboard.repository.querydsl;
+import java.util.List;
+public interface ArticleRepositoryCustom {
+    List<String> findAllDistinctHashtags();
+}
